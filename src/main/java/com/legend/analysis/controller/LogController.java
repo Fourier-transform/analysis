292fea0eac869fffdffc112a927cee21d7397072
Lj <237173983@qq.com>
2019-03-02 16:26:29 +0800

    fix

--- src/main/java/com/legend/analysis/controller/LogController.java
+++ src/main/java/com/legend/analysis/controller/LogController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * Created by Administrator on 2017/8/16.
  */
 @Controller
-@RequestMapping(value = "/user")
+@RequestMapping(value = "/")
 public class LogController {
 
     @Autowired
